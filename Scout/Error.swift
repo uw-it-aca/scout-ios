b9a3bcf782f4888851712baf73ac12924a82a0eb
Charlon Palacay <charlon@gmail.com>
2017-10-26 09:51:30 -0700

    Updates button text using customizable strings. Fixes SCOUT-710

--- Scout/Error.swift
+++ Scout/Error.swift
@@ -7,20 +7,23 @@
 
 
 struct Error {
-    static let HTTPNotFoundError = Error(title: "Page Not Found", message: "There doesn’t seem to be anything here.")
-    static let NetworkError = Error(title: "No Connection", message: "Seems like your internet connection is off. Click the Retry button to try again.")
-    static let UnknownError = Error(title: "Unknown Error", message: "An unknown error occurred.")
+    static let HTTPNotFoundError = Error(title: "Page Not Found", message: "There doesn’t seem to be anything here.", button: "Retry")
+    static let NetworkError = Error(title: "No Connection", message: "You don't seem to be connected to the internet.", button: "Try again to connect")
+    static let UnknownError = Error(title: "Unknown Error", message: "An unknown error occurred.", button: "Retry")
     
     let title: String
     let message: String
+    let button: String
     
-    init(title: String, message: String) {
+    init(title: String, message: String, button: String) {
         self.title = title
         self.message = message
+        self.button = button
     }
     
     init(HTTPStatusCode: Int) {
         self.title = "Server Error"
         self.message = "The server returned an HTTP \(HTTPStatusCode) response."
+        self.button = "Retry"
     }
 }
